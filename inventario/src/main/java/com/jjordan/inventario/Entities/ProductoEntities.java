de4db779558be43c8977ca9a714f70b301994b15
jordanEscobar <jordanescosoto@gmail.com>
2026-06-18 12:59:49 -0400

    create entity Persona

---
+++ inventario/src/main/java/com/jjordan/inventario/Entities/ProductoEntities.java
@@ -0,0 +1,70 @@
+package com.jjordan.inventario.Entities;
+
+import java.util.Date;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "producto")
+public class ProductoEntities {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nombre;
+    private int precio;
+    private int cantidad;
+    private boolean estado;
+    private Date fechaCreacion;
+
+    public Long getId(){
+        return id;
+    }
+
+    public void setId(Long id){
+        this.id = id;
+    }
+
+    public String getNombre(){
+        return nombre;
+    }
+
+    public void setNombre(String nombre){
+        this.nombre = nombre;
+    }
+
+    public int getPrecio(){
+        return precio;
+    }
+
+    public void setPrecio(int precio){
+        this.precio = precio;
+    }
+
+    public int getCantidad(){
+        return cantidad;
+    }
+
+    public void setCantidad(int cantidad){
+        this.cantidad = cantidad;
+    }
+
+    public boolean isEstado(){
+        return estado;
+    }
+
+    public void setEstado(boolean estado){
+        this.estado = estado;
+    }
+
+    public Date getFechaCreacion(){
+        return fechaCreacion;
+    }
+
+    public void setFechaCreacion(Date fechaCreacion){
+        this.fechaCreacion = fechaCreacion;
+    }
+
+}
