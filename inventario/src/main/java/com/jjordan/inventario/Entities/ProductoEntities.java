89cfc5168e8f188ac9efe3d6107e7e3eaf75ae6d
jordanEscobar <jordanescosoto@gmail.com>
2026-06-18 13:17:38 -0400

    update entity producto

--- inventario/src/main/java/com/jjordan/inventario/Entities/ProductoEntities.java
+++ inventario/src/main/java/com/jjordan/inventario/Entities/ProductoEntities.java
@@ -3,6 +3,8 @@ package com.jjordan.inventario.Entities;
 import java.util.Date;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 
